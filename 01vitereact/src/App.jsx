777cbac15cedd126c306a37c8917975d66973a63
shardulss16 <shardulss16>
2024-07-04 13:49:05 +0530

    learning vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from "./Hello"
+
+function App() {
+
+  return (
+    <>
+      <Hello />
+      <h1> Hello Shardul</h1>
+    </>
+ 
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello() {
+    return(
+        <h3>learning vite react</h3>
+    )
+}
+
+export default Hello
